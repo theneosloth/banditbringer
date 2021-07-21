4628aee62aa3713a27fd2b4f3de3f787db3983e9
Stefan Kuznetsov <neosloth@posteo.net>
2021-07-20 21:31:51 -0700

    Readme update, command test

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Bandit Bringer
 
 [Add to your server](https://discord.com/oauth2/authorize?client_id=866906317780418571&scope=bot&permissions=2147485760)
+
+## How to run
+`go install -v banditbringer/cmd/bot banditbringer/cmd/scraper `
+
+`bin/scraper && bin/bot`
---
+++ internal/commands/command_test.go
@@ -0,0 +1,62 @@
+package commands
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	commandName  = "TestCommand"
+	prefix       = "!"
+	validCommand = "!echo"
+)
+
+func TestCheckCommand(t *testing.T) {
+	testCommand := Command{
+		Name:     commandName,
+		Prefix:   prefix,
+		callback: nil,
+	}
+
+	testValid := testCommand.checkCommand(validCommand)
+
+	if !testValid {
+		t.Errorf("%s should be a valid command", validCommand)
+	}
+
+	invalidCommand := "?echo"
+	testInvalid := testCommand.checkCommand(invalidCommand)
+	if testInvalid {
+		t.Errorf("%s should be an invalid command", invalidCommand)
+	}
+}
+
+func TestRun(t *testing.T) {
+
+	mockMessage := discordgo.Message{
+		Content: validCommand,
+	}
+
+	mockMessageCreate := discordgo.MessageCreate{
+		Message: &mockMessage,
+	}
+
+	mockSession := discordgo.Session{}
+
+	testCommand := Command{
+		Name:   commandName,
+		Prefix: prefix,
+		callback: func(s *discordgo.Session, m *discordgo.MessageCreate) {
+			if len(m.Content) == 0 {
+				t.Error("passed message empty")
+			}
+			if strings.HasPrefix(m.Content, prefix) {
+				t.Error("message still has prefix")
+			}
+		},
+	}
+
+	testCommand.Run(&mockSession, &mockMessageCreate)
+}
